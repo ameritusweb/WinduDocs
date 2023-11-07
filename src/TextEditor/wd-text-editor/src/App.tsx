56aea315c692aff5d3667620c987e0d611a2d20d
Scott Seto <scott.seto@mitechnicalsolutions.com>
2023-11-07 17:11:58 -0500

    fix editor.

--- src/TextEditor/wd-text-editor/src/App.tsx
+++ src/TextEditor/wd-text-editor/src/App.tsx
@@ -1,36 +1,22 @@
-declare var Prism: typeof import('prismjs');
+declare const Prism: typeof import('prismjs');
 import './prism/prism.js'
 import './App.css'
 import { CodeEditor } from './components/code-editor.js';
 
 function App() {
 
-  const answer = '';//Prism.highlight('const a = 2;', Prism.languages.javascript, 'javascript');
+  Prism.highlight('const a = 2;', Prism.languages.javascript, 'javascript');
   return (
     <>
       <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="tabs">
-              <a className="tab tab-lifted">{answer}</a> 
-              <a className="tab tab-lifted tab-active">Tab 2</a> 
-              <a className="tab tab-lifted">Tab 3</a>
-            </div>
+            <div className="card w-[60rem] h-[90rem] bg-base-100 shadow-xl">
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Cookies!</h2>
           <div className="w-full">
-            <h1>Code Editor</h1>
             <CodeEditor />
-          </div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Accept</button>
-            <button className="btn btn-ghost">Deny</button>
           </div>
         </div>
       </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/TextEditor/wd-text-editor/src/components/code-editor.tsx
+++ src/TextEditor/wd-text-editor/src/components/code-editor.tsx
@@ -1,4 +1,4 @@
-declare var Prism: typeof import('prismjs');
+declare const Prism: typeof import('prismjs');
 import React, { useState, useRef, useEffect } from 'react';
 
 export const CodeEditor: React.FC = () => {
@@ -6,14 +6,28 @@ export const CodeEditor: React.FC = () => {
   const contentEditableRef = useRef<HTMLDivElement>(null);
   const overlayRef = useRef<HTMLElement>(null);
 
+  const createFragment = (html: string) => {
+    const el = document.createElement("div");
+    el.innerHTML = html;
+    const frag = document.createDocumentFragment();
+    let node, lastNode;
+    while ((node = el.firstChild)) {
+      lastNode = frag.appendChild(node);
+    }
+    return {frag, lastNode};
+  }
+
+  useEffect(() => {
+    if (contentEditableRef.current && !contentEditableRef.current.childNodes.length) {
+      const fragment = createFragment('\u200B');
+      contentEditableRef.current.appendChild(fragment.frag!);
+    }
+    setCode(contentEditableRef.current?.innerHTML || '');
+  }, []);
+
   // This useEffect is for applying syntax highlighting.
   useEffect(() => {
     if (overlayRef.current && contentEditableRef.current) {
-      const codeWithLineBreaks = code
-        .replace(/<br class="new-line-placeholder">/g, '\n'); // Replace placeholder with newline character
-  
-      // Set innerHTML of overlay to code with line breaks
-      overlayRef.current.innerHTML = codeWithLineBreaks;
   
       // Apply Prism syntax highlighting
       Prism.highlightElement(overlayRef.current);
@@ -27,25 +41,19 @@ export const CodeEditor: React.FC = () => {
       range.deleteContents(); // Delete any selected text
   
       // Insert the HTML fragment
-      const el = document.createElement("div");
-      el.innerHTML = html;
-      let frag = document.createDocumentFragment(), node, lastNode;
-      while ((node = el.firstChild)) {
-        lastNode = frag.appendChild(node);
-      }
-      range.insertNode(frag);
+      const fragment = createFragment(html);
+      range.insertNode(fragment.frag!);
   
       // Preserve the selection
-      if (lastNode) {
+      if (fragment.lastNode) {
         range = range.cloneRange();
-        range.setStartAfter(lastNode);
+        range.setStartAfter(fragment.lastNode);
         range.collapse(true);
         sel.removeAllRanges();
         sel.addRange(range);
       }
     }
   };
-  
   
   const handleInput = (event: React.FormEvent<HTMLDivElement>) => {
     // Capturing innerHTML to include structural HTML like <div> and <br>
@@ -54,63 +62,223 @@ export const CodeEditor: React.FC = () => {
   };
 
   function removeZeroWidthSpaces() {
-    const walker = document.createTreeWalker(
-        contentEditableRef.current!,
-        NodeFilter.SHOW_TEXT,
-        null
-      );
-      
-      let nodesToRemove = [];
-      let node;
-      while ((node = walker.nextNode())) {
+    if (contentEditableRef.current && contentEditableRef.current.childNodes && contentEditableRef.current.childNodes.length > 0)
+    {
+      const node = contentEditableRef.current.childNodes[contentEditableRef.current.childNodes.length - 1];
+      if (node.nodeValue === '' && !node.nodeValue?.includes('\u200B')) {
+        contentEditableRef.current.removeChild(node);
+      }
+    }
+
+    if (contentEditableRef.current && contentEditableRef.current.childNodes && contentEditableRef.current.childNodes.length > 0)
+      {
+        const node = contentEditableRef.current.childNodes[contentEditableRef.current.childNodes.length - 1];
         if (node.nodeValue?.includes('\u200B')) {
-          // Collect nodes that include zero-width space characters
-          nodesToRemove.push(node);
+          contentEditableRef.current.removeChild(node);
         }
       }
-      
-      // Replace each text node's value by removing all zero-width space characters
-      nodesToRemove.forEach((textNode) => {
-        if (textNode.nodeValue) {
-          textNode.nodeValue = textNode.nodeValue.replace(/\u200B/g, '');
+  }
+
+  const isAtEnd = (div: HTMLDivElement, selection: Selection): boolean => {
+    const range = selection.getRangeAt(0);
+
+    // Get content length
+    const content = div.textContent;
+    const length = content?.length;
+    
+    // Check if cursor at end
+    return range.startOffset === length;
+  }
+
+  const cleanupStart = () => {
+    if (contentEditableRef.current)
+        {
+          while (contentEditableRef.current.childNodes.length > 0 && contentEditableRef.current.lastChild && (!contentEditableRef.current.lastChild.textContent || contentEditableRef.current.lastChild.textContent.length === 0 || contentEditableRef.current.lastChild.nodeValue?.includes('\u200B')))
+          {
+            contentEditableRef.current.removeChild(contentEditableRef.current.lastChild);
+          }
+          if (contentEditableRef.current.childNodes.length === 0)
+          {
+            insertTextAtCursor('\u200B');
+          }
         }
-      });
-      
+  }
+
+  const pasteText = (text: string) => {
+    // Split text into an array of characters
+    const chars = text.split('');
+
+    // Create a document fragment to build DOM
+    const docFrag = document.createDocumentFragment();
+
+    // Create a text node for each character
+    for(const char of chars) {
+      const node = document.createTextNode(char);
+      docFrag.appendChild(node);
+    }
+
+    // Create text nodes for new lines 
+    const lines = text.split('\n');
+    lines.forEach(line => {
+      if (line) {
+        docFrag.appendChild(document.createTextNode('\n'));  
+      }
+    });
+
+    contentEditableRef.current!.appendChild(docFrag);
+  }
+
+  const handleCut = (event: React.ClipboardEvent<HTMLDivElement>) => {
+
+    if (!contentEditableRef.current)
+    {
+      return;
+    }
+
+    event.preventDefault(); // prevent default cut behavior
+
+    const selection = window.getSelection();
+    if (selection) {
+      const range = selection.getRangeAt(0);
+
+      // Get the text that would have been cut
+      const text = range.toString(); 
+
+      // Set clipboard data
+      event.clipboardData.setData('text/plain', text);
+
+      // Delete selected content
+      range.deleteContents();
+
+      cleanupStart();
+
+      setCode(contentEditableRef.current.innerHTML || '');
+    }
+
+  }
+
+  const handlePaste = (event: React.ClipboardEvent<HTMLDivElement>) => {
+    if (!contentEditableRef.current)
+    {
+      return;
+    }
+
+    event.preventDefault();
+
+    const text = event.clipboardData.getData('Text');
+
+    pasteText(text);
+
+    setCode(contentEditableRef.current.innerHTML || '');
+
   }
   
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement> & React.ClipboardEvent<HTMLDivElement>) => {
+    if (!contentEditableRef.current)
+    {
+      return;
+    }
+
+    if (event.key === 'Shift' || event.key === 'Alt' || event.key === 'Control') {
+      return;
+    }
+
+    if ((event.key === 'v' || event.key === 'x') && event.ctrlKey) {
+      return;
+    }
+
     if (event.key === 'Tab') {
       event.preventDefault();
       insertTextAtCursor('    '); // Insert four spaces for a tab
     } else if (event.key === 'Enter') {
       event.preventDefault();
       // Insert placeholder for new line which will be replaced in the overlay
-      insertTextAtCursor('<br class="new-line-placeholder">\u200B'); // Include a zero-width space after the <br> to ensure it gets rendered
+      insertTextAtCursor('\n');
+      insertTextAtCursor('\u200B'); // Include a zero-width space after the <br> to ensure it gets rendered
       // Manually trigger the update to the code state
       setCode(contentEditableRef.current?.innerHTML || '');
     }
     else if (event.key === 'Backspace') {
-        contentEditableRef.current && removeZeroWidthSpaces();
-    }
-  };
-  
+      event.preventDefault();
+      const selection = window.getSelection();
+      if (selection && selection.toString() && selection.type === 'Range') {
+        // Delete selected text
+        selection.deleteFromDocument();
+        cleanupStart();
+        setCode(contentEditableRef.current?.innerHTML || '');
+        return;
+      }
+      if (selection && !isAtEnd(contentEditableRef.current, selection)) {
 
-  const handleKeyUp = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === 'Enter') {
-      // Manually trigger the update to the code state
+        const range = selection.getRangeAt(0);
+        if (range.startOffset > 0) {
+          range.setStart(range.startContainer, range.startOffset - 1);
+          range.deleteContents();
+        }
+        else
+        {
+          if (range.startContainer.textContent?.length === 0)
+          {
+            const prev = range.startContainer.previousSibling;
+            contentEditableRef.current.removeChild(range.startContainer);
+            if (prev)
+            {
+              contentEditableRef.current.removeChild(prev);
+            }
+          }
+          else
+          {
+            contentEditableRef.current.removeChild(range.startContainer);
+          }
+        }
+        cleanupStart();
+        setCode(contentEditableRef.current?.innerHTML || '');
+
+        return;
+      }
+      if (contentEditableRef.current)
+      {
+        while (contentEditableRef.current.childNodes.length > 1 && contentEditableRef.current.lastChild && (!contentEditableRef.current.lastChild.textContent || contentEditableRef.current.lastChild.textContent.length === 0 || contentEditableRef.current.lastChild.nodeValue?.includes('\u200B')))
+        {
+          contentEditableRef.current.removeChild(contentEditableRef.current.lastChild);
+        }
+
+        if (contentEditableRef.current.childNodes.length === 1)
+        {
+          contentEditableRef.current.removeChild(contentEditableRef.current.lastChild!);
+          insertTextAtCursor('\u200B');
+          setCode(contentEditableRef.current?.innerHTML || '');
+          return;
+        }
+
+        if (contentEditableRef.current.lastChild && contentEditableRef.current.childNodes.length > 1) {
+            contentEditableRef.current.removeChild(contentEditableRef.current.lastChild);
+        }
+
+        if (contentEditableRef.current.lastChild && contentEditableRef.current.lastChild?.nodeValue === '\n') {
+          insertTextAtCursor('\u200B');
+        }
+
+        setCode(contentEditableRef.current?.innerHTML || '');
+      }
+    }
+    else if (event.key.length === 1 && (event.key === ' ' || event.key.match(/[^\s|\r|\t|\n]/gm))) {
+      event.preventDefault();
+      removeZeroWidthSpaces();
+      insertTextAtCursor(event.key);
       setCode(contentEditableRef.current?.innerHTML || '');
     }
-  };
-  
+  };  
   
   return (
-    <div style={{ position: 'relative', border: '1px solid #ccc', padding: '10px', minHeight: '100px', width: '100%' }}>
+    <div style={{ position: 'relative', padding: '10px', minHeight: '30px', width: '100%' }}>
       <div
         contentEditable
         ref={contentEditableRef}
         onInput={handleInput}
         onKeyDown={handleKeyDown}
-        onKeyUp={handleKeyUp}
+        onCut={handleCut}
+        onPaste={handlePaste}
         id="contentEditable"
         style={{
           color: 'rgba(100, 100, 100, 1)',
@@ -121,14 +289,16 @@ export const CodeEditor: React.FC = () => {
           width: '100%',
           minHeight: '100px',
           overflow: 'auto',
+          outline: 'none',
           whiteSpace: 'pre-wrap',
-          padding: '1.1rem 2rem 1rem 2.3rem',
+          padding: '1.1rem 2rem 1rem 5rem',
+          cursor: 'text',
           textAlign: 'left',
           fontSize: '1em',
           fontFamily: 'Consolas,Monaco,\'Andale Mono\',\'Ubuntu Mono\',monospace'
         }}
       ></div>
-      <pre style={{ pointerEvents: 'none', color: 'black', position: 'relative', opacity: 0.5 }}>
+      <pre className="line-numbers" style={{ pointerEvents: 'none', color: 'black', position: 'relative', opacity: 0.5 }}>
         <code id="overlay" ref={overlayRef} className="language-md">
           {code}
         </code>
